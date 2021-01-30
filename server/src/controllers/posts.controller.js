d766e0e0f62b8abb70aebbfc519670a6f9d03b71
nguyenquy42 <nguyenquy26012014@gmail.com>
2021-01-30 19:12:33 +0700

    đỗi vị trí code

--- server/src/controllers/posts.controller.js
+++ server/src/controllers/posts.controller.js
@@ -1,39 +1,6 @@
 const { update } = require('../models/post.model');
 const Post = require('../models/post.model')
 
-module.exports.createPostComment = async (req, res) => {
-
-    const { type, data } = req.body;
-    const _id = req.params
-
-    if (type === 'comments') {
-        const post = await Post.findOne({ _id })
-        post.comments.push({
-            content: data.comment
-        })
-        post.save()
-    }
-    res.json({ isSuccess: true })
-}
-
-module.exports.createPostReaction = async (req, res) => {
-    const { postUpdate } = req.body; //
-
-    const post = Post.where({ _id: req.body._id })
-    post.update({ $set: { reaction: req.body.reaction } }).exec()
-    return res.json({ isSuccess: true, data: req.body })
-    // Post.findByIdAndUpdate(req.params.id, req.body, function (err, doc) {
-    //     if (err) {
-    //         return res.json({
-    //             isSuccess: false,
-    //             message: 'Error in updating reaction',
-    //         })
-    //     }
-
-    //     return res.json({ isSuccess: true, data: req.body._id });
-    // })
-
-}
 
 module.exports.getPost = async (req, res) => {
     const posts = await Post.find()
@@ -77,4 +44,28 @@ module.exports.createPost = async (req, res) => {
             })
         }
     });
+}
+
+module.exports.createPostReaction = async (req, res) => {
+    const postUpdate = req.body; 
+
+    const post = Post.where({ _id: postUpdate._id })
+    post.update({ $set: { reaction: postUpdate.reaction } }).exec()
+    return res.json({ isSuccess: true, data: postUpdate })
+
+}
+
+module.exports.createPostComment = async (req, res) => {
+
+    const { type, data } = req.body;
+    const _id = req.params
+
+    if (type === 'comments') {
+        const post = await Post.findOne({ _id })
+        post.comments.push({
+            content: data.comment
+        })
+        post.save()
+    }
+    res.json({ isSuccess: true })
 }
